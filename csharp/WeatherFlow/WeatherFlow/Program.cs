209af218c94b9fc9e0e9404e527f670f2e496523
rs <robertsiggi@online.de>
2025-02-03 21:32:48 +0100

    cts

--- csharp/WeatherFlow/WeatherFlow/Program.cs
+++ csharp/WeatherFlow/WeatherFlow/Program.cs
@@ -1,46 +1,54 @@
 ﻿using WeatherFlow.Auth;
 using WeatherFlow.Thingsboard;
 
-Console.WriteLine("Starting");
+namespace WeatherFlow;
 
-try
+class Program
 {
-    var authService = new AuthService();
-    var response = await authService.Login();
-// Console.WriteLine(response.Token);
-
-    var permissions = await authService.Permissions(response.Token);
-
-    var client = new ThingsBoardClient(response.Token, "26945210-05ec-11ef-ac80-dde635ebcdb2");
-    var dataStream = await client.StartDataStream();
-
-    await foreach (var data in dataStream)
+    private static async Task Main()
     {
-        Console.WriteLine($"Temperature: {data.Temperature}");
+        using (var cts = new CancellationTokenSource())
+        {
+            var cancellationTask = Task.Run(() =>
+            {
+                Console.WriteLine("Press 'q' to terminate program");
+                while (true)
+                {
+                    if (Console.ReadKey(true).Key == ConsoleKey.Q)
+                    {
+                        cts.Cancel();
+                        break;
+                    }
+                }
+            }, cts.Token);
+
+            try
+            {
+                var authService = new AuthService();
+                var response = await authService.Login();
+
+                var permissions = await authService.Permissions(response.Token);
+
+                var client = new ThingsBoardClient(response.Token, "26945210-05ec-11ef-ac80-dde635ebcdb2");
+                var dataStream = await client.StartDataStream(cts.Token);
+
+                await foreach (var data in dataStream.WithCancellation(cts.Token))
+                {
+                    if (data == null)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine($"Temperature: {data.Temperature}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("program terminated");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+        }
     }
 }
-catch (Exception e)
-{
-    Console.WriteLine($"Exception: {e.Message}");
-}
-
-Console.ReadKey();
-
-// var uri = new Uri("");
-//
-// using (var ws = new ClientWebSocket())
-// {
-//     try
-//     {
-//         Console.WriteLine("Connecting...");
-//         await ws.ConnectAsync(uri, CancellationToken.None);
-//         Console.WriteLine("Connection established");
-//         
-//         
-//         
-//     }
-//     catch (Exception e)
-//     {
-//         Console.WriteLine($"Error: {e.Message}");
-//     }
-// }
